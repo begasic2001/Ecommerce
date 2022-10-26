0d83ef02a2646b785d43235ef0552831455b47d9
gx <quanhung309.ht@gmail.com>
2022-10-26 20:21:15 +0700

    [feat](HomePage) - add hot sale
    - includes: responsive, UI
    - excludes: fetching products, linking detail product

--- client/src/features/public/hot-sale/HotSale.tsx
+++ client/src/features/public/hot-sale/HotSale.tsx
@@ -1,46 +1,49 @@
 import { Stack, Container, ImageList, ImageListItem } from '@mui/material';
 import { ProductItem } from '~/components/product-item';
+import { useTheme, useMediaQuery } from '@mui/material';
 import s from './hot-sale.module.scss';
 
 const HotSaleFeature = () => {
+  const theme = useTheme();
+  const largeMedia = useMediaQuery(theme.breakpoints.up('lg'));
+  const mediumMedia = useMediaQuery(theme.breakpoints.between('sm', 'lg'));
+  const smallMedia = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <Container className={s.footer}>
       <h3 className={s['footer__title']}>Hot sale</h3>
       <Stack className={s.content}>
-        <Stack className={s.big}>
-          <section className={s['big__image']}>
-            <img
-              src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg"
-              alt="big sale flower"
-            />
-          </section>
-          <Stack className={s['big-info']}>
-            <p className={s['big-info__name']}>Hoa Tang</p>
-            <p className={s['big-info__price']}>
-              <span>$300</span> $225
-            </p>
+        {largeMedia && (
+          <Stack className={s.big}>
+            <section className={s['big__image']}>
+              <img
+                src="https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg"
+                alt="big sale flower"
+              />
+            </section>
+            <Stack className={s['big-info']}>
+              <p className={s['big-info__name']}>Hoa Tang</p>
+              <p className={s['big-info__price']}>
+                <span>$300</span> $225
+              </p>
+            </Stack>
+            <Stack className={s['big-remain']}>
+              <p className={s['big-remain__txt']}>Còn lại</p>
+              <p className={s['big-remain__time']}>43 : 22 : 17</p>
+            </Stack>
           </Stack>
-          <Stack className={s['big-remain']}>
-            <p className={s['big-remain__txt']}>Còn lại</p>
-            <p className={s['big-remain__time']}>43 : 22 : 17</p>
-          </Stack>
+        )}
+        <Stack className={s.other}>
+          <ImageList cols={mediumMedia ? 3 : smallMedia ? 2 : 4} gap={20}>
+            {Array(8)
+              .fill(0)
+              .map(() => (
+                <ImageListItem>
+                  <ProductItem />
+                </ImageListItem>
+              ))}
+          </ImageList>
         </Stack>
-        <ImageList>
-          {Array(6)
-            .fill(0)
-            .map(() => (
-              <ImageListItem>
-                <ProductItem />
-              </ImageListItem>
-            ))}
-        </ImageList>
-        {/* <S.HotOther>
-          {Array(6)
-            .fill(0)
-            .map(() => (
-              <ProductItem />
-            ))}
-        </S.HotOther> */}
       </Stack>
     </Container>
   );
